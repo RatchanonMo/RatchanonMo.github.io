f6245fb7e2f9f90cb91079c56cef24e45c530955
@Ratchanon <ratchanon.moo@dappmaker.co.th>
2024-07-19 15:10:43 +0700

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-9xl font-extrabold text-white">พอร์ตโฟลิโอ</h1>
+    </div>
+  );
+}
